8b9849ef506cfc978fc2d31d095f46399783640f
Shivam Gupta <gupta.shivam2604@gmail.com>
2023-08-20 18:12:52 +0530

    Completed Checkout ui change and implemented backend for req.body.couponid

--- index.js
+++ index.js
@@ -16,16 +16,44 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api", useRouter);
 
+
 const port = process.env.PORT || 5005;
 
 
-const path = require("path");
 
-  app.use(express.static(path.join(__dirname, "./client/build")));
+if (process.env.NODE_ENV == "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https") {
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    } else {
+      next();
+    }
+  });
+
+  const path = require("path");
+
+  app.use(
+    "/static",
+    express.static(path.join(__dirname, "client", "build/static"))
+  );
+
+  app.use(
+    "/manifest.json",
+    express.static(path.join(__dirname, "client", "build", "manifest.json"))
+  );
+
+  app.use(
+    "/favicon.ico",
+    express.static(path.join(__dirname, "client", "build", "favicon.ico"))
+  );
+
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+}
+
 
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
